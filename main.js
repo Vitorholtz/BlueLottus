7c218e68c60fc48cce4c4bb5558cb3b69b6f2e64
Vítor Holtz <vitorholtz27@gmail.com>
2021-02-01 10:54:54 -0300

    first

---
+++ main.js
@@ -0,0 +1,8 @@
+let inputButton = document.querySelector('.btn-input')
+let inputToggle = document.querySelector('.btn-input-toggle');
+
+inputButton.addEventListener('click', () => {
+
+    inputButton.classList.toggle('btn-input-active')
+
+});
